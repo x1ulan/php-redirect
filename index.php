3bcdf94702314274ed4f8b22e2ff5d3e63d306ae
x1ulan <147968053+x1ulan@users.noreply.github.com>
2024-03-25 19:00:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && $_GET !== []) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Centered Input</title>
+  <title>shorturl</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
